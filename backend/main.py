d03da15bd80ab36cc241ee57b19a6dfbbc6337f3
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2025-08-05 21:07:44 +0300

    setup docker, docker compose and precommit

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def hello():
+    return {"message": "Flight Booking API"}
