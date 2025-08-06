d30cb91f5081ad1eaf184e76985bb98350cb54bd
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2025-08-06 23:05:32 +0300

    feat(auth): register and login(jwt token) user with email and password

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from backend.routers import users
+from backend.crud.database import init_db
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def startup():
+    init_db()
+
+
+app.include_router(users.router)
 
 
 @app.get("/")
