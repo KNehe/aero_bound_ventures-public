050a4c895fe6b6ea350ac6e744254a8f0cb88b38
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2025-09-27 21:39:09 +0300

    config: create remote terraform state backend on aws s3

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -6,5 +6,13 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket          = "aero-bound-ventures-terraform-state-bucket"
+    key             = "prod/terraform.tfstate"
+    region          = "us-east-2"
+    use_lockfile    = true
+    encrypt         = true
+  }
+
   required_version = ">= 1.2.0"
 }
