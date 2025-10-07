c5e3f7539c418e34e6ef5ad5ee6c145fe96c57c5
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2025-10-07 22:04:42 +0300

    handle cors and it's configuration

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.routers import users
 from backend.crud.database import init_db
 from backend.routers import flights
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
+
+origins = os.getenv("CORS_ORIGINS", "").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
