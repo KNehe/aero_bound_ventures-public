2e9d012b49125291c2966a314b3411a0565c1d75
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2026-08-15 17:24:26 +0300

    add ingress infra, helm template, and workflow

--- terraform/kubernetes-staging/main.tf
+++ terraform/kubernetes-staging/main.tf
@@ -9,6 +9,16 @@ data "aws_subnets" "default" {
   }
 }
 
+resource "aws_ec2_tag" "public_load_balancer_subnets" {
+  for_each = toset(data.aws_subnets.default.ids)
+
+  resource_id = each.value
+  key         = "kubernetes.io/role/elb"
+  value       = "1"
+
+  depends_on = [aws_eks_cluster.staging]
+}
+
 locals {
   name = "${var.project_name}-${var.environment}"
 
